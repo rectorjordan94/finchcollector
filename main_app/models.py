539a8a12d2a5ef7f32fa0bd154d5157919a9cf67
Jordan <rectorjordan94@gmail.com>
2023-03-02 16:00:38 -0600

    creates finches model, changes to index template and view function, creates detail view function, route, and template

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    wingspan = models.CharField(max_length=50)
+    weight = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from.models import Finch
+
+# finches =[
+#     {'name': 'European Goldfinch', 'wingspan': '21-25 cm', 'weight': '14-19 g', 'description': 'Male European goldfinches can often be distinguished by a larger, darker red mask that extends just behind the eye. The shoulder feathers are black, whereas they are brown on the female. In females, the red face does not extend past the eye. The ivory-coloured bill is long and pointed, and the tail is forked.'},
+#     {'name': 'Hawfinch', 'wingspan': '29-33 cm', 'weight': '46-70 g', 'description': 'It is a robust bird with a thick neck, large round head and a wide, strong conical beak with a metallic appearance. It has short pinkish legs with a light hue and it has a short tail. It has brown eyes. The plumage of the female is slightly paler than that of the male. The overall colour is light brown, its head having an orange hue to it.'},
+# ]
 
 # Create your views here.
 
@@ -9,3 +15,11 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
