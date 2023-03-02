55e648ed2bf9fd5d163c03f5fddfcf191b9ab235
Jordan <rectorjordan94@gmail.com>
2023-03-02 14:54:35 -0600

    creates home and about view functions and templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# define the home view
+def home(request):
+    # include an.html file extension
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
